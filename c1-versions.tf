989aea3ef513ea8e5b5c23cdc66812e85a52eca0
Navedkhan787 <111653716+Navedkhan787@users.noreply.github.com>
2022-08-31 14:35:56 +0530

    Add files via upload

---
+++ c1-versions.tf
@@ -0,0 +1,29 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "ff9def2a-54b2-4ab1-ae49-d96539f8fd57"
+  client_id       = "4a543087-2b24-45ff-b3b1-8657e0fca260"
+  client_secret   =  "gc-8Q~1xAQXZQIEfOtz4F-e-I2F-TTJ0iw3NKbL-"
+  tenant_id       = "530c48cf-744a-4225-9c0f-c349cf5e9c65"
+}
+
+# Random String Resource
+resource "random_string" "myrandom" {
+  length = 6
+  upper = false 
+  special = false
+  numeric = false   
+}
+
