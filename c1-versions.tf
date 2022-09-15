1df9e4c2e53511de5af992c36f7ce2a1bea3bb8f
Navedkhan787 <111653716+Navedkhan787@users.noreply.github.com>
2022-09-15 09:33:07 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -20,10 +20,10 @@ provider "azurerm" {
 }
 
 # Random String Resource
-resource "random_string" "myrandom" {
-  length = 6
-  upper = false 
-  special = false
-  numeric = false   
-}
+#resource "random_string" "myrandom" {
+#  length = 6
+#  upper = false 
+#  special = false
+#  numeric = false   
+#}
 
