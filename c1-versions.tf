0bb1262483156bfbdd51e23d36a0aca7bb97d42d
Navedkhan787 <111653716+Navedkhan787@users.noreply.github.com>
2022-10-10 23:33:12 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -13,10 +13,10 @@ terraform {
 provider "azurerm" {
   features {}
   
-  subscription_id = "2b0bc876-a197-45f4-a31b-70b8167b9e50"
-  client_id       = "3660c1e1-2165-49e8-8457-98e2a2e277fa"
-  client_secret   =  "AHf8Q~fULuMH52duCAGa-jLJl35ee4vNveMuDaIo"
-  tenant_id       = "f55577d5-c08a-401f-9604-4bc6c5f28dc8"   
+  subscription_id = "17655825-51b0-41c3-aac1-8856d5fa4226"
+  client_id       = "57a3aa49-3427-423a-8cda-100229a80d91"
+  client_secret   =  "q7r8Q~bSTpN_RUe5dWB7r4KGaQ7wpcgGntvUNdd2"
+  tenant_id       = "1c660b68-8c5f-4a39-a6cd-caa9ad322b88"   
   
 }
 
