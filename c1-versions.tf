64633f3c4f0ecb015092f91b9c9a3d3d812b71d4
Navedkhan787 <111653716+Navedkhan787@users.noreply.github.com>
2022-10-10 23:49:32 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -14,10 +14,9 @@ provider "azurerm" {
   features {}
   
   subscription_id = "17655825-51b0-41c3-aac1-8856d5fa4226"
-  client_id       = "57a3aa49-3427-423a-8cda-100229a80d91"
-  client_secret   =  "q7r8Q~bSTpN_RUe5dWB7r4KGaQ7wpcgGntvUNdd2"
-  tenant_id       = "1c660b68-8c5f-4a39-a6cd-caa9ad322b88"   
-  
+  client_id       = "c9a4a764-64be-4099-8929-e1a1f8d512c2"
+  client_secret   =  "Pzq8Q~sAelvXjniXVXeWZksXBiRhsMLzxYHM0bDT"
+  tenant_id       = "1c660b68-8c5f-4a39-a6cd-caa9ad322b88"    
 }
 
 #Random String Resource
