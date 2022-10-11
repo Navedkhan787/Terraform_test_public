6326d2cad2d7bd5232792720fb2761ce0917fe5e
Navedkhan787 <111653716+Navedkhan787@users.noreply.github.com>
2022-10-11 12:47:18 +0530

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   
   subscription_id = "17655825-51b0-41c3-aac1-8856d5fa4226"
   client_id       = "c9a4a764-64be-4099-8929-e1a1f8d512c2"
-  client_secret   =  "Pzq8Q~sAelvXjniXVXeWZksXBiRhsMLzxYHM0bDT"
+  client_secret   =  "Pzq8Q~sAelvXjniXVXeWZksXBiRhsMLzxYHM0bDT9"
   tenant_id       = "1c660b68-8c5f-4a39-a6cd-caa9ad322b88"    
 }
 
